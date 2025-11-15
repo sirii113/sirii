a45da5da475be3dc3db9d915a4c1d717c6841c44
sirii113 <srinikitha354@gmail.com>
2025-11-15 08:42:06 +0530

    Create perfect.java

---
+++ perfect.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class Perfect
+  {
+    public static void main(String[] args)
+    {
+      Scanner myObj=new Scanner(System.in);
+      System.out.println("enter the value of n")
+        
